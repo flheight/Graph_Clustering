277ec321a3d0b18b65bec9172a3b6324a1ef8c7a
flheight <75139846+flheight@users.noreply.github.com>
2024-06-07 08:48:01 +0200

    Update test_impossible.py

--- test_impossible.py
+++ test_impossible.py
@@ -6,7 +6,7 @@ from sklearn.metrics import adjusted_rand_score
 
 np.random.seed(0)
 
-data = np.genfromtxt('impossible.csv', delimiter=',')[1:]
+data = np.genfromtxt('datasets/impossible.csv', delimiter=',')[1:]
 
 X, y = data[:, :-1], data[:, -1]
 
