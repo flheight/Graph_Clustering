cfca9256d9da9684890329a07ba032732f3e7db0
flheight <75139846+flheight@users.noreply.github.com>
2024-06-07 23:48:30 +0200

    Update graph.py

--- graph.py
+++ graph.py
@@ -11,12 +11,15 @@ class Graph:
 
         affinity = np.zeros((n_nodes, n_nodes))
 
+        tril_mask = np.tri(n_nodes, n_nodes, k=-1, dtype=bool)
+
         X_centered = [X[kmeans.labels_ == i] - kmeans.cluster_centers_[i] for i in range(n_nodes)]
+
         counts = np.array([X_centered[i].shape[0] for i in range(n_nodes)])
         counts = counts[:, np.newaxis] + counts[np.newaxis, :]
+        
         segments = kmeans.cluster_centers_[:, np.newaxis] - kmeans.cluster_centers_[np.newaxis, :]
-        dists = np.einsum('ijk,ijk->ij', segments, segments)
-        np.fill_diagonal(dists, 1)
+        dists = np.einsum('ij,ij->i', segments[tril_mask], segments[tril_mask])
 
         for i in range(1, n_nodes):
             projs_i = np.dot(X_centered[i], segments[i, :i].T)
@@ -25,7 +28,8 @@ class Graph:
                 projs_j = np.dot(X_centered[j], segments[j, i])
                 affinity[i, j] += np.maximum(projs_j, 0).sum()
 
-        affinity = np.power((affinity + affinity.T) / (counts * dists), .5)
+        affinity[tril_mask] = np.power(affinity[tril_mask] / (counts[tril_mask] * dists), .5)
+        affinity += affinity.T
 
         q1 = np.quantile(affinity, .25)
         q3 = np.quantile(affinity, .75)
