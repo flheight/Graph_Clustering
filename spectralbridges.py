888eb2d71ec41eb690479bdb197d09114d132016
flheight <75139846+flheight@users.noreply.github.com>
2024-06-14 00:45:07 +0200

    Update spectralbridges.py

--- spectralbridges.py
+++ spectralbridges.py
@@ -1,24 +1,45 @@
 import numpy as np
-from sklearn.cluster import KMeans
+from sklearn.cluster import kmeans_plusplus
 from scipy.sparse.csgraph import laplacian
 from scipy.spatial import cKDTree
+import faiss
+
+class KMeans:
+    def __init__(self, n_clusters, n_iter=20):
+        self.n_clusters = n_clusters
+        self.n_iter = n_iter
+
+    def fit(self, X):
+        index = faiss.IndexFlatL2(X.shape[1])  # Using L2 distance
+        kmeans = faiss.Clustering(X.shape[1], self.n_clusters)
+        init_centroids =  kmeans_plusplus(X, self.n_clusters)[0].astype(np.float32)
+
+        kmeans.centroids.resize(init_centroids.size)
+        faiss.memcpy(kmeans.centroids.data(), faiss.swig_ptr(init_centroids), init_centroids.size * 4)
+        kmeans.niter = self.n_iter 
+        kmeans.min_points_per_centroid = 0
+        kmeans.max_points_per_centroid = -1
+        kmeans.train(X.astype(np.float32), index)
+
+        self.cluster_centers_ = faiss.vector_to_array(kmeans.centroids).reshape(self.n_clusters, X.shape[1])
+        self.labels_ = index.search(X.astype(np.float32), 1)[1].ravel()
 
 class SpectralBridges:
-    def __init__(self, n_clusters, random_state=None):
+    def __init__(self, n_clusters):
         self.n_clusters = n_clusters
-        self.random_state = random_state
 
     def fit(self, X, n_nodes, M=1e4):
-        kmeans = KMeans(n_clusters=n_nodes, random_state=self.random_state).fit(X)
+        kmeans_pre = KMeans(n_clusters=n_nodes)
+        kmeans_pre.fit(X)
 
         affinity = np.empty((n_nodes, n_nodes))
 
-        X_centered = [X[kmeans.labels_ == i] - kmeans.cluster_centers_[i] for i in range(n_nodes)]
+        X_centered = [X[kmeans_pre.labels_ == i] - kmeans_pre.cluster_centers_[i] for i in range(n_nodes)]
 
         counts = np.array([X_centered[i].shape[0] for i in range(n_nodes)])
         counts = counts[np.newaxis, :] + counts[:, np.newaxis]
 
-        segments = kmeans.cluster_centers_[np.newaxis, :] - kmeans.cluster_centers_[:, np.newaxis]
+        segments = kmeans_pre.cluster_centers_[np.newaxis, :] - kmeans_pre.cluster_centers_[:, np.newaxis]
         dists = np.einsum('ijk,ijk->ij', segments, segments)
         np.fill_diagonal(dists, 1)
 
@@ -39,10 +60,17 @@ class SpectralBridges:
         eigvecs = np.linalg.eigh(L)[1]
         eigvecs = eigvecs[:, :self.n_clusters]
         eigvecs /= np.linalg.norm(eigvecs, axis=1)[:, np.newaxis]
-        labels = KMeans(n_clusters=self.n_clusters, random_state=self.random_state).fit_predict(eigvecs)
+        kmeans_post = KMeans(n_clusters=self.n_clusters)
+        kmeans_post.fit(eigvecs)
 
-        self.clusters = [kmeans.cluster_centers_[labels == i] for i in range(self.n_clusters)]
+        self.clusters = [kmeans_pre.cluster_centers_[kmeans_post.labels_ == i] for i in range(self.n_clusters)]
 
     def predict(self, x):
-        min_dists = np.array([cKDTree(cluster).query(x, 1)[0] for cluster in self.clusters])
+        min_dists = np.empty((self.n_clusters, x.shape[0]))
+
+        for i, cluster in enumerate(self.clusters):
+            index = faiss.IndexFlatL2(x.shape[1])
+            index.add(cluster.astype(np.float32))
+            min_dists[i] = index.search(x.astype(np.float32), 1)[1].ravel()
+
         return min_dists.argmin(axis=0)
